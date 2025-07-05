b11203587a3583463e768e86ef780b672da4cbc9
Sergey <96-94@mail.ru>
2025-07-05 15:51:14 +0300

    Новый проект, в старом я запутался не смог сделать пул рекюст

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "com.example.my"
     compileSdk = 35
 
+    buildFeatures {
+        viewBinding = true
+    }
+
     defaultConfig {
         applicationId = "com.example.my"
         minSdk = 29
